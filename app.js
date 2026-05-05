6907b4704a15b024a428688be1dd8baba86ece0f
unknown <mauro2000dom@gmail.com>
2026-05-05 19:52:03 -0300

    agregue el cards pero esta incompleto aun falta mas orden y diseño

--- app.js
+++ app.js
@@ -41,5 +41,5 @@ const toggle=document.getElementById("menu-toggle");
 const nav=document.getElementById("nav-links");
 toggle.addEventListener("click", () => {
     nav.classList.toggle("active");
-    toggle.classList.toggle("open");
+    toggle.classList.toggle("open"); 
 });
