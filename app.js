622ab139bcad0030fa04144160a7297b6152d196
lucascardozo301-rgb <lucascardozo301@gmail.com>
2026-05-23 16:17:32 -0300

    Update app.js
    Borrando todo el punto 1:casi 20 bloques de codigo

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-// 1. Efecto de Desvanecido Moderno y Parallax en el Fondo
-window.addEventListener('scroll', () => {
-    const scroll = window.pageYOffset;
-    const heroBg = document.getElementById('hero-bg');
-    const heroSection = document.getElementById('hero-section');
-    
-    if (heroSection) {
-        const height = heroSection.offsetHeight;
-        
-        // Mueve la imagen un poco más lento (Parallax)
-        heroBg.style.transform = `translateY(${scroll * 0.4}px)`;
-        
-        // Baja la opacidad suavemente según bajas el scroll
-        let opacityValue = 1 - (scroll / height);
-        
-        // No dejamos que baje de 0
-        heroBg.style.opacity = opacityValue < 0 ? 0 : opacityValue;
-    }
-});
-
 // 2. Efecto de aparición (Fade-in) de las secciones al bajar
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
